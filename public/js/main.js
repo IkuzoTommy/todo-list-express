b2ace1bc8529d41106db9218c6ef851074dbfb21
IkuzoTommy <101907933+IkuzoTommy@users.noreply.github.com>
2022-07-27 17:23:03 -0400

    todo list final comments

--- public/js/main.js
+++ public/js/main.js
@@ -25,49 +25,49 @@ async function deleteItem(){ // dispatched on click via event listener
               'itemFromJS': itemText
             })
           })
-        const data = await response.json() //awaits 'response' to be completed and stores it into the 'data' variable
+        const data = await response.json() // awaits 'response' to be completed and stores it into the 'data' variable
         console.log(data)
-        location.reload() //reloads the page on the client side
+        location.reload() // reloads the page on the client side
 
-    }catch(err){ //if try fails this executes and console logs the err for debugging
+    }catch(err){ // if try fails this executes and console logs the err for debugging
         console.log(err)
     }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+async function markComplete(){ // function inititialization, this function will allow the user to mark things complete
+    const itemText = this.parentNode.childNodes[1].innerText // references the text that is within the second child of the parent section of the current item in focus
+    try{ //tries this line of code first to carry out the function
+        const response = await fetch('markComplete', { // awaits and assigns the response of the fetch to a constant named 'response'. Also begins specifying the 'options' of the response.
+            method: 'put', // updates content
+            headers: {'Content-Type': 'application/json'}, //signifies that the response should be JSON
+            body: JSON.stringify({ // grabs object and converts it into json that can be parsed through
                 'itemFromJS': itemText
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() // waits for the fetch to be completed before storing it into another varibale 'data'.
+        console.log(data) // prints data recieved into the console
+        location.reload() // reloads the page
 
-    }catch(err){
-        console.log(err)
+    }catch(err){ // if the 'try' in unsuccessful then this code executes
+        console.log(err) // prints to the conslole what error was incurred 
     }
 }
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+async function markUnComplete(){ // function initialization that will allow the user to mark things incomplete after marking them complete.
+    const itemText = this.parentNode.childNodes[1].innerText // targets the text of the second child within the parent node of the item in focus
+    try{ //executes first
+        const response = await fetch('markUnComplete', { // awaits the response of the function and outlines specific settings of how the response should be given
+            method: 'put', //update content
+            headers: {'Content-Type': 'application/json'}, // should be a json object when the response is given.
+            body: JSON.stringify({ // grabs item and converts the object into json that is able to be parsed through
                 'itemFromJS': itemText
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() // awaits the response of the function and resulting json and stores it into a variable 'data'.
+        console.log(data) // prints the response to the console
+        location.reload() //reloads the page
 
-    }catch(err){
-        console.log(err)
+    }catch(err){ // executes only if the 'try' doesnt work
+        console.log(err) // console logs the error type to the console for debugging
     }
 }
