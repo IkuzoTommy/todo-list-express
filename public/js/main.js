6ad7e4332f18cb53df3093600bb24e9671dda108
IkuzoTommy <101907933+IkuzoTommy@users.noreply.github.com>
2022-07-27 21:42:34 -0400

    partner progrmming

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,18 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
+const deleteBtn = document.querySelectorAll('.fa-trash') //assigns a varible that will allow js to target the html element with the specified class
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+//creates an array from the all of the delete item buttons
 Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+    element.addEventListener('click', deleteItem) // adds an event listener to each item's delete button that will execute the deleteItem function.
+})
+//creates an array from every list item in class item.
+Array.from(item).forEach((element)=>{ //creates an array from each list item
+    element.addEventListener('click', markComplete) // runs the function when the user clicks the span with the class 'item'.
 })
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
-
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+Array.from(itemCompleted).forEach((element)=>{ // when items are marked completed they are sorted seperately and in an array.
+    element.addEventListener('click', markUnComplete) // assigns an event listener to items specifically in this class to beable to later unmark them as complete.
 })
 
 async function deleteItem(){
